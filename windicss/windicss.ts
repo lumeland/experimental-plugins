6409f671ead2a98f4b786c73fe333690cfe6d337
dragonwocky <thedragonring.bod@gmail.com>
2021-12-15 17:59:16 +1100

    windi: output in <style> tag opt

--- windicss/windicss.ts
+++ windicss/windicss.ts
@@ -1,17 +1,36 @@
 import { extname } from "lume/deps/path.ts";
+import { merge } from "lume/core/utils.ts";
 import { SitePage } from "lume/core/filesystem.ts";
 import { Page, Site } from "lume/core.ts";
 import { CSSParser, HTMLParser, Processor, StyleSheet } from "./deps.ts";
 
-type Mode = "interpret" | "compile";
 export interface Options {
-  minify?: boolean;
-  mode?: Mode;
-  output?: string;
+  minify: boolean;
+  mode: "interpret" | "compile";
+  output: {
+    // ouput mode "file"
+    // = a single file will be created with generated styles
+    //   from across the entire site
+    // output mode "styleTag"
+    // = a <style> tag will be inserted into each page
+    //   containing only the generated styles for that page
+    mode: "file" | "styleTag";
+    filename?: string;
+  };
   // https://github.com/windicss/windicss/blob/main/src/interfaces.ts#L367
   // https://windicss.org/guide/configuration.html
-  config?: Record<string, unknown>;
+  config: Record<string, unknown>;
 }
+
+const defaults = {
+  minify: false,
+  mode: "interpret" as "interpret" | "compile",
+  output: {
+    mode: "file" as "file" | "styleTag",
+    filename: "windi.css",
+  },
+  config: {},
+};
 
 /**
  * a lume plugin for windicss, the next generation utility-first css framework
@@ -22,48 +41,50 @@ export interface Options {
  * the output css file must be manually included in your document's
  * head e.g. <link rel="stylesheet" href="/windi.css">
  */
-export default function ({
-  minify = false,
-  mode = "interpret",
-  output = "/windi.css",
-  config = {},
-}: Options = {}) {
+export default function (userOptions: Partial<Options> = {}) {
+  const options = merge(defaults, userOptions) as Options;
+
   return (site: Site) => {
     // create & configure a windicss instance
     // (config assignment merges provided with defaults)
     const processor = new Processor();
-    config = processor.loadConfig(config);
+    options.config = processor.loadConfig(options.config);
 
     site.addEventListener("afterRender", () => {
-      // create & merge stylesheets for all pages
-      const stylesheet = site.pages
-        .filter((page) => page.dest.ext === ".html")
-        .map((page) => windi(page, processor, mode, config))
-        .reduce(
-          (previous, current) => previous.extend(current, false),
-          new StyleSheet(),
-        )
-        .sort()
-        .combine();
+      const pages = site.pages
+        .filter((page) => page.dest.ext === ".html");
 
-      // output css as a page
-      const ext = extname(output),
-        path = output.slice(0, -ext.length),
-        page = new SitePage({ path, ext });
-      page.content = stylesheet.build(minify);
-      site.pages.push(page);
+      if (options.output.mode === "file" && options.output.filename) {
+        // create & merge stylesheets for all pages
+        const stylesheet = pages
+          .map((page) => windi(page, processor, options))
+          .reduce(
+            (previous, current) => previous.extend(current),
+            new StyleSheet(),
+          )
+          .sort()
+          .combine();
+
+        // output css as a page
+        const ext = extname(options.output.filename),
+          path = options.output.filename.slice(0, -ext.length),
+          page = new SitePage({ path, ext });
+        page.content = stylesheet.build(options.minify);
+        site.pages.push(page);
+      } else if (options.output.mode === "styleTag") {
+        // insert stylesheets directly into pages
+        for (const page of pages) {
+          const stylesheet = windi(page, processor, options);
+          page.content += `<style>${stylesheet.build(options.minify)}</style>`;
+        }
+      }
     });
   };
 }
 
-export function windi(
-  page: Page,
-  processor: Processor,
-  mode: Mode,
-  config: Record<string, unknown>,
-) {
-  const content = page.content as string,
-    parser = new HTMLParser(content);
+export function windi(page: Page, processor: Processor, options: Options) {
+  let content = page.content as string;
+  const parser = new HTMLParser(content);
 
   // update page content with classnames output from windi
   // e.g. to expand variant:(class groups) and to support compile mode
@@ -71,20 +92,20 @@ export function windi(
   for (const className of parser.parseClasses()) {
     html += content.substring(index, className.start);
     index = className.end;
-    if (mode === "interpret") {
+    if (options.mode === "interpret") {
       const interpreted = processor.interpret(className.result);
       html += [...interpreted.success, ...interpreted.ignored].join(" ");
       stylesheet = stylesheet.extend(interpreted.styleSheet);
-    } else if (mode === "compile") {
+    } else if (options.mode === "compile") {
       const compiled = processor.compile(
         className.result,
-        config.prefix as string,
+        options.config.prefix as string || "windi-",
       );
       html += [compiled.className, ...compiled.ignored].join(" ");
       stylesheet = stylesheet.extend(compiled.styleSheet);
     }
   }
-  page.content = html + content.substring(index);
+  content = html + content.substring(index);
 
   // attributify: https://windicss.org/features/attributify.html
   // reduceRight taken from https://github.com/windicss/windicss/blob/main/src/cli/index.ts
@@ -109,19 +130,22 @@ export function windi(
   stylesheet = stylesheet.extend(attributified.styleSheet);
 
   // style blocks: use @apply etc. in a style tag
+  // will always replace the inline style block with the generated styles
   // https://windicss.org/features/directives.html
   // https://windicss.org/posts/language.html
   // https://windicss.org/integrations/cli.html#style-block
-  const block = content.match(
-    /(?<=<style lang=['"]windi["']>)[\s\S]*(?=<\/style>)/,
+  content = content.replace(
+    /<style lang=['"]windi["']>([\s\S]*)<\/style>/,
+    (_match, css) => {
+      return `<style>${
+        new CSSParser(css, processor).parse().build(options.minify)
+      }</style>`;
+    },
   );
-  if (block && block.index) {
-    const css = content.slice(block.index, block.index + block[0].length),
-      styleBlock = new CSSParser(css, processor);
-    stylesheet = stylesheet.extend(styleBlock.parse());
-  }
 
-  if (!config.preflight) return stylesheet;
+  page.content = content;
+
+  if (!options.config.preflight) return stylesheet;
   const preflightSheet = processor.preflight(content);
   return stylesheet.extend(preflightSheet);
 }
