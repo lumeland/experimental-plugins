962d6e0014413a07720b7fd3bd2582a301f0eff8
dragonwocky <thedragonring.bod@gmail.com>
2021-12-16 22:42:47 +1100

    windi: import local dom dep

--- windicss/deps.ts
+++ windicss/deps.ts
@@ -5,7 +5,4 @@ export {
 } from "https://esm.sh/windicss@3.3.0/utils/parser";
 export { StyleSheet } from "https://esm.sh/windicss@3.3.0/utils/style";
 
-export {
-  Element,
-  HTMLDocument,
-} from "https://deno.land/x/deno_dom@v0.1.19-alpha/deno-dom-wasm.ts";
+export { Element, HTMLDocument } from "lume/deps/dom.ts";
