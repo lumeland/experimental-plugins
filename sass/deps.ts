8d93db8462cb66e3ca53abb631bbaa06a2ea53c0
Oscar Otero <oom@oscarotero.com>
2022-02-14 13:49:47 +0100

    updated sass

--- sass/deps.ts
+++ sass/deps.ts
@@ -1,2 +1,5 @@
-export { default as denosass } from "https://deno.land/x/denosass@1.0.2/mod.ts";
-export type { SassFormats } from "https://deno.land/x/denosass@1.0.2/src/types/module.types.ts";
+export { str } from "https://deno.land/x/denosass@1.0.3.1/src/wasm/grass.deno.js";
+export type {
+  SassFormats,
+  SassOptions,
+} from "https://deno.land/x/denosass@1.0.3.1/src/types/module.types.ts";
--- sass/sass.ts
+++ sass/sass.ts
@@ -1,17 +1,24 @@
 import { merge } from "lume/core/utils.ts";
-import { denosass, SassFormats } from "./deps.ts";
+import { SassFormats, SassOptions, str } from "./deps.ts";
+import { dirname } from "lume/deps/path.ts";
 
 import type { Page, Site } from "lume/core.ts";
 
 export interface Options {
-  /** Set `true` to include the style reset */
+  /** Extensions processed by this plugin */
   extensions: string[];
+
+  /** Output format */
   format: SassFormats;
+
+  /** Custom includes paths */
+  includes: string | string[];
 }
 
 const defaults: Options = {
   extensions: [".sass", ".scss"],
   format: "compressed",
+  includes: "_sass",
 };
 
 /** A plugin to use SASS in Lume */
@@ -19,17 +26,26 @@ export default function (userOptions?: Partial<Options>) {
   const options = merge(defaults, userOptions);
 
   return (site: Site) => {
+    const includes = Array.isArray(options.includes)
+      ? options.includes.map((path) => site.src(path))
+      : [site.src(options.includes)];
+
     site.loadAssets(options.extensions);
     site.process(options.extensions, sass);
 
     function sass(page: Page) {
-      const compiler = denosass(page.content as string);
-      const result = compiler.to_string(options.format);
+      const code = page.content as string;
+      const filename = site.src(page.src.path + page.src.ext);
+      const sassOptions: SassOptions = {
+        load_paths: [...includes, dirname(filename)],
+        style: options.format,
+        quiet: false,
+      };
 
-      if (typeof result === "string") {
-        page.content = result;
-        page.dest.ext = ".css";
-      }
+      const result = str(code, sassOptions);
+
+      page.content = result;
+      page.dest.ext = ".css";
     }
   };
 }
