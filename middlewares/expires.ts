e87b7095e8e8898055f58a6daa512d3ab530c985
Oscar Otero <oom@oscarotero.com>
2022-03-04 18:38:33 +0100

    optional argument

--- middlewares/expires.ts
+++ middlewares/expires.ts
@@ -25,7 +25,7 @@ export const defaults: Options = {
 };
 
 /** Set the Expires header for better caching */
-export default function expires(userOptions: Partial<Options>): Middleware {
+export default function expires(userOptions?: Partial<Options>): Middleware {
   const options = { ...defaults, ...userOptions };
 
   return async (request, next) => {
