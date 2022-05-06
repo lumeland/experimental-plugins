77d9d7c6bb737605ae055e6b826902af9c9e96b1
Oscar Otero <oom@oscarotero.com>
2022-05-07 00:31:46 +0200

    added a filter by default

--- google_analytics/mod.ts
+++ google_analytics/mod.ts
@@ -1,11 +1,22 @@
 import { createReporter } from "./deps.ts";
+import { merge } from "lume/core/utils.ts";
 
 import type { Middleware, RequestHandler } from "lume/core.ts";
 import type { ReporterOptions } from "./deps.ts";
 
 export type Options = ReporterOptions;
 
-export default function GoogleAnalytics(options?: Options): Middleware {
+export const defaults: Options = {
+  filter(_request, response) {
+    return response.status === 200 &&
+      !!response.headers.get("content-type")?.includes("text/html");
+  },
+};
+
+export default function GoogleAnalytics(
+  userOptions?: Partial<Options>,
+): Middleware {
+  const options = merge(defaults, userOptions);
   const reporter = createReporter(options);
 
   return async (request: Request, next: RequestHandler, connInfo) => {
