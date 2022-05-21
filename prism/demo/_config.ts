696abdd1dfb882a7605eec6f792f1f4ac1039656
Oscar Otero <oom@oscarotero.com>
2022-05-21 15:54:36 +0200

    removed prism

--- prism/demo/_config.ts
+++
@@ -1,9 +0,0 @@
-import lume from "lume/mod.ts";
-import prism from "../mod.ts";
-
-const site = lume();
-
-site
-  .use(prism());
-
-export default site;
--- prism/mod.ts
+++ prism/mod.ts
@@ -1,39 +1,5 @@
-import Prism, { loadLanguages } from "./deps.ts";
-import { merge } from "lume/core/utils.ts";
-
-import type { Page, Site } from "lume/core.ts";
-import type { Element } from "lume/deps/dom.ts";
-
-export interface Options {
-  /** The list of extensions this plugin applies to */
-  extensions: string[];
-  /** The css selector to apply prism */
-  cssSelector: string;
-  /** The list of programing languages to load */
-  languages: string[];
-}
-
-// Default options
-export const defaults: Options = {
-  extensions: [".html"],
-  cssSelector: "pre code",
-  languages: [],
-};
-
-/** A plugin to syntax-highlight code using the prism library */
-export default function (userOptions?: Partial<Options>) {
-  const options = merge(defaults, userOptions);
-
-  return (site: Site) => {
-    site.addEventListener(
-      "beforeBuild",
-      () => loadLanguages(options.languages),
-    );
-    site.process(options.extensions, prism);
-
-    function prism(page: Page) {
-      page.document!.querySelectorAll(options.cssSelector!)
-        .forEach((element) => Prism.highlightElement(element as Element));
-    }
-  };
-}
+throw new Error(`
+  Module moved to the main lume repository.
+  Update lume to the latest version
+  and import this plugin from "lume/plugins/prism.ts".
+`);
