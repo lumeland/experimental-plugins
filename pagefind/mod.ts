6dcb3cb5e20eca0034c5ab75b97c6bb07b87fd50
Oscar Otero <oom@oscarotero.com>
2022-09-12 00:30:27 +0200

    added pagefind plugin

---
+++ pagefind/mod.ts
@@ -0,0 +1,140 @@
+import { merge } from "lume/core/utils.ts";
+import { posix } from "lume/deps/path.ts";
+
+import type { Site } from "lume/core.ts";
+
+export interface Options {
+  ui?: {
+    /** The container id to insert the search */
+    containerId: string;
+
+    /** Whether to show an image alongside each search result. */
+    showImages: boolean;
+
+    /**
+     * By default, Pagefind UI shows filters with no results alongside the count (0).
+     * Pass false to hide filters that have no remaining results.
+     */
+    showEmptyFilters: boolean;
+
+    /**
+     * By default, Pagefind UI applies a CSS reset to itself.
+     * Pass false to omit this and inherit from your site styles.
+     */
+    resetStyles: boolean;
+  } | false;
+
+  indexing: {
+    /** The folder to output search files into, relative to source. */
+    bundleDirectory: string;
+
+    /** The element that Pagefind should treat as the root of the document. */
+    rootSelector: string;
+
+    /** Configures the glob used by Pagefind to discover HTML files. */
+    glob: string;
+
+    /** Ignores any detected languages and creates a single index for the entire site as the provided language. */
+    forceLanguage: string | false;
+
+    /** Prints extra logging while indexing the site. */
+    verbose: boolean;
+  };
+}
+
+const defaults: Options = {
+  ui: {
+    containerId: "search",
+    showImages: false,
+    showEmptyFilters: true,
+    resetStyles: true,
+  },
+  indexing: {
+    bundleDirectory: "_pagefind",
+    rootSelector: "html",
+    glob: "**/*.html",
+    forceLanguage: false,
+    verbose: false,
+  },
+};
+
+/** A plugin to generate a static full text search engine */
+export default function (userOptions?: Partial<Options>) {
+  const options = merge(defaults, userOptions);
+
+  return (site: Site) => {
+    const { ui, indexing } = options;
+
+    if (ui) {
+      site.process([".html"], (page) => {
+        const container = page.document?.getElementById(ui.containerId);
+        const uiSettings = {
+          element: `#${ui.containerId}`,
+          showImages: ui.showImages,
+          showEmptyFilters: ui.showEmptyFilters,
+          resetStyles: ui.resetStyles,
+          bundlePath: site.url(posix.join(indexing.bundleDirectory, "/")),
+        };
+
+        // Insert UI styles and scripts
+        if (container) {
+          const styles = page.document!.createElement("link");
+          styles.setAttribute("rel", "stylesheet");
+          styles.setAttribute(
+            "href",
+            site.url(
+              `${posix.join(indexing.bundleDirectory, "pagefind-ui.css")}`,
+            ),
+          );
+
+          const script = page.document!.createElement("script");
+          script.setAttribute("type", "text/javascript");
+          script.setAttribute(
+            "src",
+            site.url(
+              `${posix.join(indexing.bundleDirectory, "pagefind-ui.js")}`,
+            ),
+          );
+
+          const init = page.document!.createElement("script");
+          init.setAttribute("type", "text/javascript");
+          init.innerHTML =
+            `window.addEventListener('DOMContentLoaded', () => { new PagefindUI(${
+              JSON.stringify(uiSettings)
+            }); });`;
+          page.document!.head.append(styles, script, init);
+        }
+      });
+    }
+
+    site.addEventListener("afterBuild", () => {
+      const command = buildCommand(options.indexing, site.dest());
+      site.run(command);
+    });
+  };
+}
+
+function buildCommand(options: Options["indexing"], source: string): string {
+  const args = [
+    "npx",
+    "pagefind",
+    "--source",
+    `"${source}"`,
+    "--bundle-dir",
+    `"${options.bundleDirectory}"`,
+    "--root-selector",
+    `"${options.rootSelector}"`,
+    "--glob",
+    `"${options.glob}"`,
+  ];
+
+  if (options.forceLanguage) {
+    args.push("--force-language", options.forceLanguage);
+  }
+
+  if (options.verbose) {
+    args.push("--verbose");
+  }
+
+  return args.join(" ");
+}
