25176b60b391111feb9977fe0150f1b9f175e0c2
Oscar Otero <oom@oscarotero.com>
2022-09-11 22:10:02 +0200

    removed minify deprecated plugin

--- minify/minify.ts
+++ minify/minify.ts
@@ -1,49 +1,6 @@
-import { Exception } from "lume/core/errors.ts";
-import { merge } from "lume/core/utils.ts";
-import { minify as rawMinify, minifyHTML, MinifyHTMLOptions } from "./deps.ts";
+console.error(
+  "This module has been moved to Lume.",
+  "Please update your import url to 'lume/plugins/minify_html.ts'",
+);
 
-import type { Page, Site } from "lume/core.ts";
-
-export interface Options {
-  /** Set `true` to include the style reset */
-  extensions: string[];
-  htmlOptions: Partial<MinifyHTMLOptions>;
-}
-
-const defaults: Options = {
-  extensions: [".html"],
-  htmlOptions: {
-    minifyCSS: false,
-    minifyJS: false,
-  },
-};
-
-/** A plugin to use SASS in Lume */
-export default function (userOptions?: Partial<Options>) {
-  const options = merge(defaults, userOptions);
-
-  return (site: Site) => {
-    site.process(options.extensions, minify);
-
-    function minify(page: Page) {
-      const content = page.content as string;
-      const language = page.dest.ext.slice(1);
-
-      switch (language) {
-        case "html":
-          page.content = minifyHTML(content, options.htmlOptions);
-          return;
-
-        case "css":
-        case "js":
-        case "json": {
-          page.content = rawMinify(language, content);
-          return;
-        }
-
-        default:
-          throw new Exception("Unsupported file type", { page });
-      }
-    }
-  };
-}
+export { default as default } from "lume/plugins/minify_html.ts";
