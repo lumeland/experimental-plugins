70f7f27d44772d37bb3800e77826991d08bcc0e3
Oscar Otero <oom@oscarotero.com>
2022-11-20 16:47:16 +0100

    removed remark

--- remark/remark.ts
+++
@@ -1,6 +0,0 @@
-console.error(
-  "This module has been moved to Lume.",
-  "Please update your import url to 'lume/middlewares/remark.ts'",
-);
-
-export { default as default } from "lume/middlewares/remark.ts";
