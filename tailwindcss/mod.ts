47941ff05361b8fcbddcd1dab21a02aa4c3ce43c
Oscar Otero <oom@oscarotero.com>
2023-06-09 18:43:11 +0200

    removed tailwindcss plugin

--- tailwindcss/mod.ts
+++
@@ -1,6 +0,0 @@
-console.error(
-  "This module has been moved to Lume.",
-  "Please update your import url to 'lume/plugins/tailwindcss.ts'",
-);
-
-export { default as default } from "lume/plugins/tailwindcss.ts";
