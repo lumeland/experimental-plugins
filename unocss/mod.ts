571cee72118ff6ef0dacab8c4c2fb905656e0563
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-15 14:16:45 +0800

    add css file option

--- unocss/mod.ts
+++ unocss/mod.ts
@@ -11,6 +11,12 @@ export interface Options {
    */
   config?: UserConfig;
   /**
+   * Set the css filename for all generated styles,
+   * Set to `false` to insert a <style> tag per page.
+   * @defaultValue `false`
+   */
+  cssFile: false | string;
+  /**
    * Supported CSS reset options.
    * @see {@link https://github.com/unocss/unocss/tree/v0.53.5/packages/reset}
    * @defaultValue `tailwind`
@@ -22,39 +28,47 @@ export const defaults: Options = {
   config: {
     presets: [presetUno()],
   },
+  cssFile: false,
   reset: "tailwind",
 };
 
 export default (userOptions: DeepPartial<Options> = {}) => {
   const options = merge(defaults, userOptions) as Options;
 
-  const uno = new UnoGenerator(options.config);
-
   return (site: Site) => {
-    site.process([".html"], async (page) => {
-      const { css } = await uno.generate(
-        page.document?.documentElement?.innerHTML ?? "",
-      );
 
-      if (options.reset !== false) {
-        const style = page.document!.createElement("style");
-        /**
-         * TODO: Replace with CSS Modules Import
-         * @remarks Deno does not currently support CSS Modules.
-         * @see {@link https://github.com/denoland/deno/issues/11961}
-         */
-        style.innerText = await read(
-          `https://unpkg.com/@unocss/reset@0.53.5/${options.reset}.css`,
-          false,
+    const uno = new UnoGenerator(options.config);
+
+    if (options.cssFile === false) {
+      site.process([".html"], async (page) => {
+        const { css } = await uno.generate(
+          page.document?.documentElement?.innerHTML ?? "",
         );
-        page.document?.head?.appendChild(style);
-      }
 
-      if (css) {
-        const style = page.document!.createElement("style");
-        style.innerText = css;
-        page.document?.head?.appendChild(style);
-      }
-    });
+        if (options.reset !== false) {
+          const style = page.document!.createElement("style");
+          /**
+           * TODO: Replace with CSS Modules Import
+           * @remarks Deno does not currently support CSS Modules.
+           * @see {@link https://github.com/denoland/deno/issues/11961}
+           */
+          style.innerText = await read(
+            `https://unpkg.com/@unocss/reset@0.53.5/${options.reset}.css`,
+            false,
+          );
+          page.document?.head?.appendChild(style);
+        }
+
+        if (css) {
+          const style = page.document!.createElement("style");
+          style.innerText = css;
+          page.document?.head?.appendChild(style);
+        }
+      });
+    } else {
+      site.addEventListener("afterRender", () => {
+        // TODO
+      })
+    }
   };
 };
