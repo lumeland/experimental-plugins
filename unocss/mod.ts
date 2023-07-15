42687f490f8569e0eb4594b0bc3f9cea7adc31eb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-15 21:05:24 +0800

    add comments

--- unocss/mod.ts
+++ unocss/mod.ts
@@ -51,6 +51,7 @@ export default (userOptions: DeepPartial<Options> = {}) => {
     const uno = new UnoGenerator(options.config);
 
     if (options.cssFile === false) {
+      // Insert a <style> tag for each page
       site.process([".html"], async (page) => {
         const css = await uno.generate(
           page.document?.documentElement?.innerHTML ?? "",
@@ -63,6 +64,7 @@ export default (userOptions: DeepPartial<Options> = {}) => {
         }
       });
     } else {
+      // Insert a <link> tag for each page
       site.process([".html"], (page) => {
         const link = page.document!.createElement("link");
         link.setAttribute("rel", "stylesheet");
@@ -70,6 +72,7 @@ export default (userOptions: DeepPartial<Options> = {}) => {
         page.document?.head?.appendChild(link);
       });
 
+      // Generate the stylesheets for all pages
       site.addEventListener("afterRender", async () => {
         const pages = site.pages
           .filter((page) => page.outputPath?.endsWith(".html"));
@@ -94,6 +97,7 @@ export default (userOptions: DeepPartial<Options> = {}) => {
           }
         }
 
+        // Create & merge stylesheets for all pages
         const css = await uno.generate(classes).then(({ css }) =>
           prependReset(css)
         );
