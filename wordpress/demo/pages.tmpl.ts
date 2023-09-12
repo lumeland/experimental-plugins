78e8e40204fad66d548215e31f4e36d0d890642f
Đăng Tú <68631458+ngdangtu-vn@users.noreply.github.com>
2023-09-12 09:01:38 +0700

    cleanup chore

--- wordpress/demo/pages.tmpl.ts
+++ wordpress/demo/pages.tmpl.ts
@@ -1,22 +1,3 @@
-// export default async function* ({ wp }) {
-//   for await (const page of wp.posts()) {
-//     yield { ...page, layout: "post.njk" };
-//   }
-//   for await (const page of wp.authors()) {
-//     yield { ...page, layout: "author.njk" };
-//   }
-//   for await (const page of wp.tags()) {
-//     yield { ...page, layout: "tag.njk" };
-//   }
-//   for await (const page of wp.categories()) {
-//     yield { ...page, layout: "category.njk" };
-//   }
-//   for await (const page of wp.pages()) {
-//     if (page.url === "/") continue; // Skip the home page (it's already generated)
-//     yield { ...page, layout: "page.njk" };
-//   }
-// }
-
 export default async function* ({ wp }) {
   // api_path: /wp/v2/posts
   for await (const page of wp.collection("posts")) {
