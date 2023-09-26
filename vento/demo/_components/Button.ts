5851239eb4ae93611ab7ac51f12180bd95d03634
Oscar Otero <oom@oscarotero.com>
2023-09-26 17:40:32 +0200

    removed experimental Vento plugin

--- vento/demo/_components/Button.ts
+++
@@ -1,8 +0,0 @@
-interface Props {
-  content: string;
-  href: string;
-}
-
-export default function ({ content, href }: Props) {
-  return `<a href="${href}" class="btn btn-primary">${content}</a>`;
-}
