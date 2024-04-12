ce152beaedead8381e9157684823a391f70fa1d7
Oscar Otero <oom@oscarotero.com>
2024-04-12 17:35:10 +0200

    removed jimp plugin

--- jimp/demo/_config.ts
+++
@@ -1,9 +0,0 @@
-import lume from "lume/mod.ts";
-import jimp from "../mod.ts";
-
-const site = lume();
-
-site
-  .use(jimp());
-
-export default site;
--- jimp/deps.ts
+++
@@ -1,2 +0,0 @@
-export { default as Jimp } from "https://esm.sh/jimp@0.16.1";
-export { Buffer } from "https://deno.land/std@0.127.0/node/buffer.ts";
