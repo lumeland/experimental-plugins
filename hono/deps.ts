3878bd10962d2534bbd93a2cb054b177babd21b3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-01 20:21:21 +0800

    feat(hono): html helper

--- hono/deps.ts
+++ hono/deps.ts
@@ -1,2 +1,4 @@
 export * from "https://deno.land/x/hono@v4.2.9/jsx/jsx-runtime.ts";
+// export { css, cx, keyframes, viewTransition, Style } from "https://deno.land/x/hono@v4.2.9/helper/css/index.ts";
+export { html } from "https://deno.land/x/hono@v4.2.9/helper/html/index.ts";
 export const specifier = "hono/jsx";
