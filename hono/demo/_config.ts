f6c0ddf3cd13962c1db398b967f387bd65ee228d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-01 22:09:42 +0800

    chore(hono): clean demo

--- hono/demo/_config.ts
+++ hono/demo/_config.ts
@@ -1,15 +1,8 @@
 import lume from "lume/mod.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
-import honojsx from "../mod.ts";
+import hono from "../mod.ts";
 
-const site = lume();
-
-site.loadAssets([".css"]);
-site.use(honojsx());
-site.use(
-  codeHighlight({
-    extensions: [".jsx", ".tsx", ".html"],
-  }),
-);
-
-export default site;
+export default lume()
+  .loadAssets([".css"])
+  .use(hono())
+  .use(codeHighlight());
