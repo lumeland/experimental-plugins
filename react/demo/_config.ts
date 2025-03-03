52b3b6f8372e9d58b8a60c1bf3b382a4e78693ae
qustrolabe <qustrolabe@gmail.com>
2025-03-03 14:38:09 +0200

    Added style.css
    Signed-off-by: qustrolabe <qustrolabe@gmail.com>

--- react/demo/_config.ts
+++ react/demo/_config.ts
@@ -1,7 +1,8 @@
 import lume from "lume/mod.ts";
 import react from "../mod.ts";
+import lightningcss from "lume/plugins/lightningcss.ts";
 
 const site = lume();
 site.use(react());
-
+site.use(lightningcss());
 export default site;
