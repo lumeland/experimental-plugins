b32f1003abad2a43948abb73d5fc728e6eea393f
Volpeon <git@volpeon.ink>
2026-05-20 15:11:58 +0200

    New plugin: downloader

---
+++ downloader/mod.ts
@@ -0,0 +1,76 @@
+import type Site from "lume/core/site.ts";
+
+import { merge } from "lume/core/utils/object.ts";
+import { isUrl } from "lume/core/utils/path.ts";
+import { read } from "lume/core/utils/read.ts";
+import { posix } from "lume/deps/path.ts";
+import modifyUrls from "lume/plugins/modify_urls.ts";
+
+export interface Options {
+  /** Domains to download images from */
+  origins: string[];
+
+  /** The folder where the images will be saved */
+  folder: string;
+
+  /** CSS selector the element needs to match */
+  selector: string;
+
+  /** Attribute to use as selector. Unlike with `selector`, it's possible to assign the attribute a value to manually define a target file name. */
+  attribute: string;
+}
+
+export const defaults: Options = {
+  origins: [],
+  folder: "/_assets",
+  selector: "img,source,use",
+  attribute: "",
+};
+
+export function downloader(userOptions?: Partial<Options>) {
+  const options = merge(defaults, userOptions);
+
+  return (site: Site) => {
+    site.use(modifyUrls({ fn: download }));
+
+    async function download(path: string, _page: Lume.Page, element?: Element) {
+      const [pathAndQuery, frag] = path.split("#", 2);
+
+      if (
+        !isUrl(path) ||
+        !pathAndQuery ||
+        !options.origins.includes(new URL(path).host) ||
+        (element && options.selector && !element.matches(options.selector))
+      ) {
+        return path;
+      }
+
+      let url;
+
+      if (options.attribute) {
+        if (element && !element.matches(`[${options.attribute}]`)) {
+          return path;
+        }
+
+        url = element?.getAttribute(options.attribute);
+      }
+
+      const content = await read(pathAndQuery, true);
+
+      if (!url) {
+        const filename = posix.basename(pathAndQuery);
+        const hash = await crypto.subtle.digest("SHA-1", content);
+        const hashHex = new Uint8Array(hash).toHex();
+        url = posix.join(options.folder, `${hashHex}-${filename}`);
+      }
+
+      const page = await site.getOrCreatePage(url);
+      page.content = content;
+      page.src.ext = posix.extname(path);
+
+      return frag ? `${url}#${frag}` : url;
+    }
+  };
+}
+
+export default downloader;
