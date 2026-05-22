272dda08b3556d08f3f1b28796c6d78d8af7a203
Oscar Otero <oom@oscarotero.com>
2026-05-23 01:12:26 +0200

    removed unused variables

--- webawesome/demo/_config.ts
+++ webawesome/demo/_config.ts
@@ -2,6 +2,6 @@ import lume from "lume/mod.ts";
 import webawesome from "../mod.ts";
 
 const site = lume();
-site.use(webawesome())
+site.use(webawesome());
 
 export default site;
--- webawesome/mod.ts
+++ webawesome/mod.ts
@@ -2,108 +2,25 @@ import { merge } from "lume/core/utils/object.ts";
 import Site from "lume/core/site.ts";
 import { posix } from "lume/deps/path.ts";
 
-const allTags = [
-  "wa-animated-image",
-  "wa-animation",
-  "wa-avatar",
-  "wa-badge",
-  "wa-bar-chart",
-  "wa-breadcrumb",
-  "wa-breadcrumb-item",
-  "wa-bubble-chart",
-  "wa-button",
-  "wa-button-group",
-  "wa-callout",
-  "wa-card",
-  "wa-carousel",
-  "wa-carousel-item",
-  "wa-chart",
-  "wa-checkbox",
-  "wa-color-picker",
-  "wa-combobox",
-  "wa-comparison",
-  "wa-copy-button",
-  "wa-details",
-  "wa-dialog",
-  "wa-divider",
-  "wa-doughnut-chart",
-  "wa-drawer",
-  "wa-dropdown",
-  "wa-dropdown-item",
-  "wa-file-input",
-  "wa-format-bytes",
-  "wa-format-date",
-  "wa-format-number",
-  "wa-icon",
-  "wa-include",
-  "wa-input",
-  "wa-intersection-observer",
-  "wa-line-chart",
-  "wa-markdown",
-  "wa-mutation-observer",
-  "wa-number-input",
-  "wa-option",
-  "wa-page",
-  "wa-pie-chart",
-  "wa-polar-area-chart",
-  "wa-popover",
-  "wa-popup",
-  "wa-progress-bar",
-  "wa-progress-ring",
-  "wa-qr-code",
-  "wa-radar-chart",
-  "wa-radio",
-  "wa-radio-group",
-  "wa-rating",
-  "wa-relative-time",
-  "wa-resize-observer",
-  "wa-scatter-chart",
-  "wa-scroller",
-  "wa-select",
-  "wa-skeleton",
-  "wa-slider",
-  "wa-sparkline",
-  "wa-spinner",
-  "wa-split-panel",
-  "wa-switch",
-  "wa-tab",
-  "wa-tab-group",
-  "wa-tab-panel",
-  "wa-tag",
-  "wa-textarea",
-  "wa-toast",
-  "wa-toast-item",
-  "wa-tooltip",
-  "wa-tree",
-  "wa-tree-item",
-  "wa-video",
-  "wa-video-playlist",
-  "wa-zoomable-frame",
-];
-
-const specifier = "npm:@awesome.me/webawesome@3.7.0/dist-cdn"
+const specifier = "npm:@awesome.me/webawesome@3.7.0/dist-cdn";
 
 export interface Options {
-  outputPath?: string,
-  theme?: "default",
-  styles?: "native" | "utilities" | "all",
+  outputPath?: string;
 }
 
 export const defaults: Options = {
   outputPath: "webawesome",
-  theme: "default",
-  styles: "all",
 };
 
 export default function (userOptions?: Options) {
   const options = merge(defaults, userOptions);
-  const { outputPath, theme, styles } = options;
+  const { outputPath } = options;
 
   return (site: Site) => {
     // Copy all assets
     site.copy(`${specifier}/**/*{.js,.css}`, outputPath);
 
-    site.process([".html"], async (pages) => {
+    site.process([".html"], (pages) => {
       for (const page of pages) {
         const { document } = page;
 
